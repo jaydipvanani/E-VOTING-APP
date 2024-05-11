759464e342307d45b215d7cc772a672e254ea820
jaydip <vananijaydip53@gmail.com>
2024-05-11 12:44:53 +0530

    E-VOTING COMPLETE

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes, Navigate, useLocation } from "react-router-dom";
+import Login from "./components/Login";
+import Dashboard from "./Admin/Pages/Dashboard";
+import Party from "./Admin/Pages/Party";
+import Election from "./Admin/Pages/Election";
+import Connection from "./Admin/Pages/Connection";
+import User from "./Admin/Pages/User";
+import AdminNav from "./Admin/Header/AdminNav";
+import UserNav from "./User/Header/UserNav";
+import Home from "./User/Pages/Home";
+import LoginUser from "./components/LoginUser";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import { useDispatch } from "react-redux";
+import {
+  GET_ALL_CONNECTION_PENDING,
+  GET_ALL_ELECTION_PENDING,
+  GET_ALL_PARTY_PENDING,
+  GET_ALL_USER_PENDING,
+} from "./redux-saga/admin/action/Action";
+import { useEffect } from "react";
+import { election_get_req, party_get_req, partylist_get_req, user_get_req } from "./redux-saga/Constant";
+
+const getRole = () => {
+  return localStorage.getItem("role");
+};
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({ type: GET_ALL_ELECTION_PENDING, endpoint: election_get_req });
+    dispatch({ type: GET_ALL_PARTY_PENDING, endpoint: party_get_req });
+    dispatch({ type: GET_ALL_CONNECTION_PENDING, endpoint: partylist_get_req });
+    dispatch({ type: GET_ALL_USER_PENDING, endpoint: user_get_req });
+  }, []);
+
+  const role = getRole();
+  const location = useLocation();
+
+  const isLoginPage = location.pathname === "/";
+
+  if (!role || role === "") {
+    return (
+      <Routes>
+        <Route path="/" element={<LoginUser />} />
+        <Route path="/adminlogin" element={<Login />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    );
+  }
+
+  if (role === "admin") {
+    return (
+      <>
+        <AdminNav />
+        <div
+          className="container"
+          style={{
+            maxWidth: "1150px",
+            padding: "30px 40px 40px",
+            height: "100vh",
+            marginLeft: "310px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Routes>
+            <Route path="*" element={<Dashboard />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/party" element={<Party />} />
+            <Route path="/election" element={<Election />} />
+            <Route path="/connection" element={<Connection />} />
+            <Route path="/user" element={<User />} />
+          </Routes>
+        </div>
+      </>
+    );
+  }
+
+  if (role === "user") {
+    return (
+      <>
+        <UserNav />
+        <div
+          className="container"
+          style={{
+            maxWidth: "1150px",
+            padding: "30px 40px 40px",
+            height: "100vh",
+            marginLeft: "310px",
+          }}
+        >
+          <Routes>
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
-}
+};
 
 export default App;
